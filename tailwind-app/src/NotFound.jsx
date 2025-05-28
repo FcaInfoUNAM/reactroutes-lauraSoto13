43fe7a068b57d7efbc7a4eb45006043ddb651136
lauraSoto13 <147668740+lauraSoto13@users.noreply.github.com>
2025-05-28 05:05:25 +0000

    propiedades de los componentes

---
+++ tailwind-app/src/NotFound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-subtitle">La página no se encontró</p>
+        <p className="not-found-description">Vuelva a intentarlo</p>
+      </div>
+    );
+  }
+
+  export default NotFound;
